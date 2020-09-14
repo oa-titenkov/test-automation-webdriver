58c157a6af0ec978f24cca670d91aa588a7b3e37
sohumble <lightning07@yandex.ru>
2020-09-14 14:23:27 +0300

    added failed test

--- src/test/java/googlecloudtasks/test/GoogleCloudCalculatorTest.java
+++ src/test/java/googlecloudtasks/test/GoogleCloudCalculatorTest.java
@@ -1,5 +1,6 @@
 package googlecloudtasks.test;
 
+import googlecloudtasks.model.ComputeEngine;
 import googlecloudtasks.page.GoogleCloudCalculatorEstimatedPage;
 import googlecloudtasks.page.GoogleCloudPage;
 import googlecloudtasks.service.ComputeEngineService;
@@ -8,31 +9,44 @@ import org.testng.annotations.Test;
 
 public class GoogleCloudCalculatorTest extends CommonConditions {
 
-  private static final String SEARCH_INPUT_TEXT = "Google Cloud Platform Pricing Calculator";
-  private static final String MANUALLY_CALCULATED_PRICE = "1,082.77";
+    private static final String SEARCH_INPUT_TEXT = "Google Cloud Platform Pricing Calculator";
+    private static final String MANUALLY_CALCULATED_PRICE = "1,082.77";
 
-  @Test(description = "Hurt Me Plenty")
-  public void calculatedPriceAndParametersAreCorrect() {
-    GoogleCloudCalculatorEstimatedPage calculatedPage = new GoogleCloudPage(driver)
+    @Test(description = "Hurt Me Plenty")
+    public void calculatedPriceAndParametersAreCorrect() {
+        GoogleCloudCalculatorEstimatedPage calculatedPage = new GoogleCloudPage(driver)
+                .openPage()
+                .searchForInput(SEARCH_INPUT_TEXT)
+                .openCalculator()
+                .calculateComputeEnginePrice(new ComputeEngineService().hardcoreTaskFormFill());
+        Assert.assertTrue(calculatedPage.checkEstimatedParameters());
+        Assert.assertEquals(calculatedPage.checkEstimatedPrice().split(" ")[4], MANUALLY_CALCULATED_PRICE);
+    }
+
+    @Test(description = "Hardcore")
+    public void calculatedPriceFromEmailEqualsManuallyCalculatedPrice() {
+        String calculatedPrice = new GoogleCloudPage(driver)
+                .openPage()
+                .searchForInput(SEARCH_INPUT_TEXT)
+                .openCalculator()
+                .calculateComputeEnginePrice(new ComputeEngineService().hardcoreTaskFillFromProperty())
+                .sendEstimatedByEmail()
+                .checkForCorrectPriceInEmail();
+        Assert.assertEquals(calculatedPrice, MANUALLY_CALCULATED_PRICE);
+    }
+
+    @Test(description = "Failed Hardcore")
+    public void failCalculatedPriceFromEmailTest() {
+        ComputeEngine wrongEngine = new ComputeEngineService().hardcoreTaskFillFromProperty();
+        wrongEngine.setNumberOfInstances("8");
+        String calculatedPrice = new GoogleCloudPage(driver)
             .openPage()
             .searchForInput(SEARCH_INPUT_TEXT)
             .openCalculator()
-            .calculateComputeEnginePrice(new ComputeEngineService().hardcoreTaskFormFill());
-    Assert.assertTrue(calculatedPage.checkEstimatedParameters());
-    Assert.assertEquals(calculatedPage.checkEstimatedPrice().split(" ")[4], MANUALLY_CALCULATED_PRICE);
-  }
-
-  @Test(description = "Hardcore")
-  public void calculatedPriceFromEmailEqualsManuallyCalculatedPrice() {
-    String calculatedPrice = new GoogleCloudPage(driver)
-            .openPage()
-            .searchForInput(SEARCH_INPUT_TEXT)
-            .openCalculator()
-            .calculateComputeEnginePrice(new ComputeEngineService().hardcoreTaskFillFromProperty())
+            .calculateComputeEnginePrice(wrongEngine)
             .sendEstimatedByEmail()
             .checkForCorrectPriceInEmail();
-    Assert.assertEquals(calculatedPrice, MANUALLY_CALCULATED_PRICE);
-  }
-
+        Assert.assertEquals(calculatedPrice, MANUALLY_CALCULATED_PRICE);
+    }
 
 }
